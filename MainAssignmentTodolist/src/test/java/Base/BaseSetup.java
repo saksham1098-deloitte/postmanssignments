4249095e4fbb699a2d2672b1dd88fea07c8de720
Saksham1908-Deloitte <saksshukla@deloitte.com>
2022-11-08 19:38:50 +0530

    second commit UserTest class created  successfully

--- MainAssignmentTodolist/src/test/java/Base/BaseSetup.java
+++ MainAssignmentTodolist/src/test/java/Base/BaseSetup.java
@@ -1,14 +1,25 @@
 package Base;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.hamcrest.CoreMatchers;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
+
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 import  static org.hamcrest.MatcherAssert.assertThat;
 
 public class BaseSetup {
-    @BeforeTest
-    public void verifycontentType()
+  public void verifyContentType(Response response)
+  {
+      assertThat(response.contentType(),containsString(String.valueOf(ContentType.HTML)));
+
+  }
+    public void verifyStatusCode(Response response)
     {
-        //assertThat(response.conteType(),containsString(String.valueOf(ContentType.JSON)));
+        assertThat(response.statusCode(),CoreMatchers.is(HttpStatus.SC_OK));
+
     }
+
 }
--- MainAssignmentTodolist/src/test/java/Testing/UserTest.java
+++ MainAssignmentTodolist/src/test/java/Testing/UserTest.java
@@ -1,22 +1,60 @@
 package Testing;
 
 import Base.BaseSetup;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasItems;
+
 public class UserTest extends BaseSetup {
-@Test
+@Test (priority = 1)
     public void RegisterUserTest()
 {
+   Response response= given().
 
+            baseUri("https://api-nodejs-todolist.herokuapp.com").
+            when().
+            get("/user/register").
+            then().log().all().
+            extract().response();
+   verifyContentType(response);
 }
-@Test
+@Test (priority = 2)
     public void LoginUserTest()
 {
 
 }
-@Test
+@Test (priority = 3)
     public void LogoutUserTest()
 {
 
 }
+@Test (priority = 4)
+    public void UpdateProfileTest()
+{
+
+}
+@Test (priority = 5)
+    public void UploadImageTest()
+{
+
+}
+@Test (priority = 6)
+    public void GetUserImageTest()
+    {
+
+    }
+
+    @Test (priority = 7)
+    public void DeleteImageTest()
+    {
+
+    }
+    @Test (priority = 8)
+    public void DeleteUserTest()
+    {
+
+    }
 }
